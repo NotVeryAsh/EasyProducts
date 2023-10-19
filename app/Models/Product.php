36319798008dd0a1f96f4896316c8efa0bf79590
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-10-19 20:56:40 +0100

    Add relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
