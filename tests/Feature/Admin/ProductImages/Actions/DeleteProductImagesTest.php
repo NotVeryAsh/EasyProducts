6afce03185b220a43433b269631c62a91236e473
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-10-19 20:48:52 +0100

    Add tests to delete and store product images£

---
+++ tests/Feature/Admin/ProductImages/Actions/DeleteProductImagesTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Admin\Products\Actions;
+
+use Tests\TestCase;
+
+class DeleteProductImagesTest extends TestCase
+{
+}
---
+++ tests/Feature/Admin/ProductImages/Actions/StoreProductImagesTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Admin\Products\Actions;
+
+use Tests\TestCase;
+
+class StoreProductImagesTest extends TestCase
+{
+}
