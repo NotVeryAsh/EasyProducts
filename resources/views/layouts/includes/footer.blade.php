915c2bde705d765d35313fb5a201ec145f2ff150
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-10-22 09:33:20 +0100

    make footer responsive

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -2,8 +2,8 @@
 
 <!-- Footer -->
 <div class="bg-primary">
-    <div class="mt-12 w-11/12 mx-auto flex flex-row">
-        <div class="w-6/12 py-6">
+    <div class="mt-12 w-12/12 mx-auto flex flex-row">
+        <div class="lg:w-6/12 py-6">
             <div class="text-center text-neutral-100 leading-relaxed px-8">
                 Copyright © 2023 Smoothies Plus - All Rights Reserved.
             </div>
