ca9f3376e662833036f36868ecfbd82ef6a5b01f
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-10-22 09:29:04 +0100

    Make find us page responsive

--- resources/views/home/includes/find_us.blade.php
+++ resources/views/home/includes/find_us.blade.php
@@ -1,14 +1,14 @@
 <!-- About Us -->
 <h1 class="mt-20 text-center font-semibold text-5xl" id="find-us">Find Us</h1>
 <div class="bg-primary">
-    <div class="mt-12 w-9/12 mx-auto flex flex-row">
-        <iframe class="w-5/10"
+    <div class="mt-12 lg:w-10/12 xl:w-9/12 mx-auto flex flex-col md:flex-row">
+        <iframe class="w-full md:w-5/10 mx-auto"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2418.4642440025873!2d-2.0197672233086568!3d52.687711872105595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487a759941c6316d%3A0x7d0c192bbfa3e5ee!2sSmoothies-plus!5e0!3m2!1sen!2sus!4v1697902468351!5m2!1sen!2sus"
-                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                height="450" allowfullscreen="" loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade">
         </iframe>
-        <div class="w-7/12">
-            <div class="text-center mt-4 text-neutral-100 leading-relaxed px-8">
+        <div class="py-8 md:py-0 md:w-7/12 flex justify-center items-center">
+            <div class="text-center mt-4 text-neutral-100 leading-relaxed md:px-8">
                 <h2 class="text-4xl font-semibold">Address</h2>
                 <p class="py-5">You can find us at the McArthurGlen Designer Outlet in Cannock.</p>
                 <p>Our address is:</p>
