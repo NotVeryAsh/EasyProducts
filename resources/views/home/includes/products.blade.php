840fda7bed3212a1515955d5b5fef22c8405b863
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-10-22 11:38:00 +0100

    Make products responsive

--- resources/views/home/includes/products.blade.php
+++ resources/views/home/includes/products.blade.php
@@ -1,73 +1,74 @@
 <!-- Products -->
 <h1 class="mt-12 text-center font-semibold text-5xl">Products</h1>
-<div class="mt-16 flex flex-row w-9/12 mx-auto">
-    <div class="w-full text-center">
+<div
+    class="mt-16 w-10/12 mx-auto flex flex-wrap gap-x-20 justify-center">
+    <div class="text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/smoothie.jpg') }}"
                 alt="Smoothie">
             <p class="font-semibold">Smoothies</p>
         </a>
     </div>
-    <div class="w-full text-center">
+    <div class="text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/milkshake.jpg') }}"
                 alt="Milshake">
             <p class="font-semibold">Milkshakes</p>
         </a>
     </div>
-    <div class="w-full text-center">
+    <div class="text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/ice_cream.jpg') }}"
                 alt="Ice Cream">
             <p class="font-semibold">Ice Cream</p>
         </a>
     </div>
-    <div class="w-full text-center">
+    <div class=" text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/doggie_ice_cream.jpg') }}"
                 alt="Ice Cream">
             <p class="font-semibold">Doggie Ice Cream</p>
         </a>
     </div>
-    <div class="w-full text-center">
+    <div class="text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/drinks.jpg') }}"
                 alt="Drinks">
             <p class="font-semibold">Drinks</p>
         </a>
     </div>
-    <div class="w-full text-center">
+    <div class="text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/waffle_pops.jpg') }}"
                 alt="Waffle Pops">
             <p class="font-semibold">Waffle Pops</p>
         </a>
     </div>
-    <div class="w-full text-center">
+    <div class="text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/cakes.jpg') }}"
                 alt="Waffle Pops">
             <p class="font-semibold">Cakes</p>
         </a>
     </div>
-    <div class="w-full text-center">
+    <div class="text-center pb-10">
         <a href="" class="space-y-3">
             <img
-                class="object-cover ring-2 ring-primary rounded-md w-24 h-24 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+                class="object-cover ring-2 ring-primary rounded-md w-28 h-28 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
                 src="{{ \App\Services\ImageService::getPublicImage('home/product_thumbnails/protein_porridge.jpg') }}"
                 alt="Protein Porridge">
             <p class="font-semibold">Protein Porridge</p>
